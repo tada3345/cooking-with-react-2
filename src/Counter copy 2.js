b9b80dd6a21c287fe142e6804dfd140ba73ee41b
tada3345 <tada3345@gmail.com>
2022-08-15 13:09:39 +0900

    First Commit

---
+++ src/Counter copy 2.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+export default class Counter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count:this.props.initialCount
+        }
+    }
+    render(){
+        return(
+            <div>
+            <button onClick={()=>this.changeCount(-1)}>-</button>
+            <span>{this.state.count}</span>
+            <button onClick={()=>this.changeCount(+1)}>+</button>
+            </div>
+        )
+    }
+    changeCount(amt){
+        this.setState(prevState=>{
+            return{count:prevState.count+amt}
+        })
+    }
+}
